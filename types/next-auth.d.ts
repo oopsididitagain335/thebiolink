f7c790503b73a534127318fd85cb928c871f0b3e
Solace <monkdev9@gmail.com>
2025-10-06 03:00:01 +0100

    Update next-auth.d.ts

--- types/next-auth.d.ts
+++ types/next-auth.d.ts
@@ -1,29 +1,26 @@
 // types/next-auth.d.ts
-import 'next-auth';
-import 'next-auth/jwt';
+import { DefaultSession, DefaultUser } from "next-auth";
 
-declare module 'next-auth' {
-  interface User {
+declare module "next-auth" {
+  interface User extends DefaultUser {
     id: string;
-    username: string;
-    name?: string | null;
-    email?: string | null;
+    username?: string;
   }
-  interface Session {
+
+  interface Session extends DefaultSession {
     user: {
       id: string;
-      username: string;
-      name?: string | null;
-      email?: string | null;
+      email: string;
+      name?: string;
+      username?: string;
     };
   }
 }
 
-declare module 'next-auth/jwt' {
+declare module "next-auth/jwt" {
   interface JWT {
     id: string;
-    username: string;
-    name?: string | null;
-    email?: string | null;
+    email: string;
+    username?: string;
   }
 }
